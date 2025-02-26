5f930ee6157b2f2932a588e59ec526e48d1a498d
Tayyabakhatri <Tayyabakhatri225@gmail.com>
2025-02-26 23:51:13 +0500

    formated

--- src/Components/OurServices/Services.jsx
+++ src/Components/OurServices/Services.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import s1 from '../../assets/ourServices1.webp'
+import s1 from "../../assets/ourServices1.webp";
 import { IoIosArrowForward } from "react-icons/io";
 
 const Services = () => {
@@ -7,13 +7,16 @@ const Services = () => {
     <>
       <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
         <a href="#">
-          <img
-            className="p-4 rounded-t-lg"
-            src={s1}
-            alt="product image"
-          />
+          <img className="p-4 rounded-t-lg" src={s1} alt="product image" />
         </a>
-        <a href="#" className="px-4 flex justify-between items-center"><p className="text-start text-[18px] font-[700]">Website Design && Development</p> <div ><IoIosArrowForward /></div></a>
+        <a href="#" className="px-4 flex justify-between items-center">
+          <p className="text-start text-[18px] font-[700]">
+            Website Design && Development
+          </p>{" "}
+          <div>
+            <IoIosArrowForward />
+          </div>
+        </a>
       </div>
     </>
   );
